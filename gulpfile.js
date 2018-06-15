b06df90d435d8a7e51dc80ebfb97053c2d1a9420
Dan <danpeltier@gmail.com>
2018-06-14 22:21:20 -0400

    gulp watcher added and site linked to auto-compiled custom.css file

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+(function () {
+   'use strict';
+   // this function is strict...
+}());
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('app/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('app/scss/*.scss', ['sass']);
+});
